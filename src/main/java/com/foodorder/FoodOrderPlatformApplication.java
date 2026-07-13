3a69cb6c754ed636d4c66d3af7320b9e28805e68
ZhongwenLee <3685297744@qq.com>
2026-07-13 22:32:07 +0800

    init: scaffold spring boot project

---
+++ src/main/java/com/foodorder/FoodOrderPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.foodorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOrderPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodOrderPlatformApplication.class, args);
+	}
+
+}
